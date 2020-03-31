3a9ff0a8bf808729cb23ef65bc104badf8afdbd6
happyhil <simonvreugdenhil@gmail.com>
2020-03-31 08:02:40 +0200

    devs on making full working scrapen + shot maker

---
+++ run/insert_market_shot.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+
+import json
+from os import path
+from datetime import datetime
+from src import pipes
+
+def main():
+
+    # today = datetime.now().strftime('%Y_%m_%d')
+    today = '2020_03_28'
+
+    # read in way data
+    with open(f'data/raw/housing_data_{today}.json', 'r') as s:
+        housesdict = json.load(s)
+
+    # prep data for calculations
+    housesdf = pipes.prepare_housing_dict(housesdict)
+
+    # run calculations and build the dict
+    full_dict = pipes.build_full_dict(housesdf)
+
+    # create the output dict
+    output_dict = {
+        today: {
+            'version': 0.1,
+            'housing_facts': full_dict
+        }
+    }
+
+    # check if current file exists
+    if path.isfile('data/housing_market_shots.json'):
+        # append data to existing json
+        with open('data/shots/housing_market_shots.json', 'r') as shotstream:
+            shots = json.load(shotstream)
+            shots.update(output_dict)
+        with open('data/shots/housing_market_shots.json', 'w') as shotstream:
+            json.dump(shots, shotstream, indent=2)
+    else:
+        # write new json
+        with open('data/shots/housing_market_shots.json', 'w') as shotstream:
+            json.dump(output_dict, shotstream, indent=2)
+
+if __name__ == '__main__':
+    main()
---
+++ src/pipes.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import re
+
+
+def strip_int_column(dataf, columns):
+    for c in columns:
+        cvalues = list(dataf[c].values)
+        cvalues = [re.sub('[^0-9]','', x) for x in cvalues]
+        dataf[f'{c}_clean'] = [0 if y == '' else int(y) for y in cvalues]
+    return dataf
+
+
+def prepare_housing_dict(dicty):
+
+    # merge dict into pandas frame
+    housesdf = pd.DataFrame.from_dict(dicty, orient='index')
+
+    # get city
+    housesdf['city'] = housesdf['location'].str.split(',').str[1]
+    housesdf['city'] = housesdf['city'].fillna('noaddress')
+
+    # convert given columns to ints
+    housesdf = strip_int_column(housesdf, ['price', 'meters'])
+
+    # divide extreem high prices by 1000 to correct for dtype error in source
+    housesdf.loc[lambda x: x['price_clean']>99999999, 'price_clean'] = housesdf['price_clean']/1000
+
+    # calculate meter price on row level and replace failed calculation with 0
+    housesdf['meter_price'] = housesdf['price_clean'] / housesdf['meters_clean']
+    housesdf['meter_price'] = housesdf['meter_price'].replace([np.inf, -np.inf], 0)
+
+    # filter on available houses only & houses with at least the field city filled
+    housesdf = housesdf.loc[lambda x: x['status']=='available']
+    housesdf = housesdf.loc[lambda x: x['location']!='noaddress']
+
+    return housesdf
+
+
+def housing_fact_dict(housesdf, area):
+
+    # calculate metrics
+    n_houses = housesdf['city'].count()
+    average_price = round(housesdf.loc[lambda x: (x['price_clean']>0)]['price_clean'].mean(), 2)
+    median_price = round(housesdf.loc[lambda x: (x['price_clean']>0)]['price_clean'].median(), 2)
+    average_meters = round(housesdf.loc[lambda x: (x['meters_clean']>0)]['meters_clean'].mean(), 2)
+    median_meters = round(housesdf.loc[lambda x: (x['meters_clean']>0)]['meters_clean'].median(), 2)
+    average_meter_price = round(housesdf.loc[lambda x: (x['meter_price']>0)]['meter_price'].mean(), 2)
+    median_meter_price = round(housesdf.loc[lambda x: (x['meter_price']>0)]['meter_price'].median(), 2)
+
+    # create dict
+    houses_dict = {
+        area:
+        {
+            'number_available': int(n_houses),
+            'price_mean': float(average_price),
+            'price_median': float(median_price),
+            'meters_mean': float(average_meters),
+            'meters_median': float(median_meters),
+            'meter_price_mean': float(average_meter_price),
+            'meter_price_median': float(median_meter_price)
+        }
+    }
+
+    return houses_dict
+
+
+def build_full_dict(housesdf):
+
+    # run totals
+    overall_dict = housing_fact_dict(housesdf, area='The Netherlands')
+
+    # run on cities
+    cities = ['Amsterdam', 'Rotterdam', 'Utrecht', 'Hilversum', 'Amstelveen']
+    cities_dicts = {}
+    for city in cities:
+        # filter on city
+        city_housesdf = housesdf.loc[lambda x: x['city'].str.contains(city)]
+        # create city sum
+        sub_city_dict = housing_fact_dict(city_housesdf, area=city)
+        # update cities
+        cities_dicts.update(sub_city_dict)
+
+    # merge dicts
+    full_dict = {**overall_dict, **cities_dicts}
+
+    return full_dict
